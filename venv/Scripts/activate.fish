9a0f162f3b4cf1b3fa019d97455dbfcfda5eb762
yassine <yassine@gmail.com>
2025-05-07 11:19:21 +0100

    V5

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\dahan\Documents\Stage PFE DXC cdg\Flask_Backend\venv'
+set -gx VIRTUAL_ENV 'C:\Users\dahan\Documents\Stage PFE DXC cdg\Project\Backend\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
